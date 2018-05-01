3658d52cd9d7e65ba34e76e22afc2e8970e30948
Changwan Jun <im@wani.kr>
2018-05-02 03:29:52 +0900

    단순 게임 룰 적용

--- frontend/entry.ts
+++ frontend/entry.ts
@@ -40,7 +40,63 @@ function act(player: entity.Player): number {
 
   // 게임 진행하기
   async function startGame(): Promise<void> {
-    //
+    const turnedPlayers = _.shuffle(players.slice())
+    let victoryPlayer: entity.Player | undefined
+    for (const turnedPlayer of turnedPlayers) {
+      await new Promise(resolve => setTimeout(resolve, 300))
+      let xToMove: number = turnedPlayer.point.x
+      let yToMove: number = turnedPlayer.point.y
+      let foundPlayer: entity.Player | undefined
+      switch (act(turnedPlayer)) {
+        case 0:
+          console.log(turnedPlayer.name, "은 위로 이동하기를 시전하였다.")
+          xToMove = turnedPlayer.point.x
+          yToMove = turnedPlayer.point.y - 1
+          turnedPlayer.point.d = entity.Direction.Up
+          break
+        case 1:
+          console.log(turnedPlayer.name, "은 오른쪽으로 이동하기를 시전하였다.")
+          xToMove = turnedPlayer.point.x + 1
+          yToMove = turnedPlayer.point.y
+          turnedPlayer.point.d = entity.Direction.Right
+          break
+          case 2:
+          console.log(turnedPlayer.name, "은 아래로 이동하기를 시전하였다.")
+          xToMove = turnedPlayer.point.x
+          yToMove = turnedPlayer.point.y + 1
+          turnedPlayer.point.d = entity.Direction.Down
+          break
+        case 3:
+          console.log(turnedPlayer.name, "은 왼쪽으로 이동하기를 시전하였다.")
+          xToMove = turnedPlayer.point.x - 1
+          yToMove = turnedPlayer.point.y
+          turnedPlayer.point.d = entity.Direction.Left
+          break
+      }
+      if (!map.isMovablePoint(xToMove, yToMove)) {
+        console.log("그러나 갈 수 있는 길이 없다.")
+        continue
+      }
+      foundPlayer = players.find(player => {
+        return player.point.x === xToMove
+          && player.point.y === yToMove
+      })
+      if (foundPlayer) {
+        console.log(`그러나 플레이어 ${foundPlayer.name}(이)가 막고 있어서 갈수 없다.`)
+        continue
+      }
+      turnedPlayer.point.x = xToMove
+      turnedPlayer.point.y = yToMove
+      if (map.isEndPoint(xToMove, yToMove)) {
+        victoryPlayer = turnedPlayer
+        break
+      }
+    }
+    if (victoryPlayer) {
+      console.log(`게임 끝! ${victoryPlayer.name} 승리!!`)
+    } else {
+      setTimeout(startGame, 1200) // next turn!
+    }
   }
   startGame()
 

ed1ab3f9686419ed80a724775a483a9d628e92c8
Changwan Jun <im@wani.kr>
2018-05-04 22:51:26 +0900

    Push 룰 적용

--- frontend/entity/player.ts
+++ frontend/entity/player.ts
@@ -2,7 +2,15 @@
 import axios from "axios"
 import {PointAndDirection} from "./point-and-direction"
 import {Map} from "./map"
+import {Direction} from "./direction"
 import * as api from "../api"
+import * as _ from "lodash"
+
+function findPlayers(others: Player[], x: number, y: number): Player | undefined {
+  return others.find(other => {
+    return other.point.x === x && other.point.y === y
+  })
+}
 
 export class Player {
 
@@ -15,7 +23,9 @@ export class Player {
     public point: PointAndDirection) {
   }
 
-  public async strategy(map: Map, others: Player[]): Promise<number> {
+  public async strategy(map: Map, players: Player[]): Promise<number> {
+    // return _.random(0, 3)
+    const others = players.filter(player => player.name !== this.name)
     let params = [
       map.width,
       map.height,
@@ -30,5 +40,63 @@ export class Player {
     })
     const action = await axios.post(`/api/players/${this.name}/action?arguments=${params.join(",")}`)
     return action.data.player.response
+  }
+
+  public move(direction: number, map: Map, players: Player[], pushed: boolean = false): boolean {
+    const others = players.filter(player => player.name !== this.name)
+    let xToMove = this.point.x
+    let yToMove = this.point.y
+    const action = pushed ? "밀리기를 당했다!!" : "이동하기를 시전하였다."
+    switch (direction) {
+      case 0:
+        console.log(`${this.name}은(는) 위로 ${action}`)
+        xToMove = this.point.x
+        yToMove = this.point.y - 1
+        if (!pushed) {
+          this.point.d = Direction.Up
+        }
+        break
+      case 1:
+        console.log(`${this.name}은(는) 오른쪽으로 ${action}`)
+        xToMove = this.point.x + 1
+        yToMove = this.point.y
+        if (!pushed) {
+          this.point.d = Direction.Right
+        }
+        break
+      case 2:
+        console.log(`${this.name}은(는) 아래로 ${action}`)
+        xToMove = this.point.x
+        yToMove = this.point.y + 1
+        if (!pushed) {
+          this.point.d = Direction.Down
+        }
+        break
+      case 3:
+        console.log(`${this.name}은(는) 왼쪽으로 ${action}`)
+        xToMove = this.point.x - 1
+        yToMove = this.point.y
+        if (!pushed) {
+          this.point.d = Direction.Left
+        }
+        break
+    }
+    if (!map.isMovablePoint(xToMove, yToMove)) {
+      if (pushed) {
+        console.log("그러나 더이상 밀릴 수 없다....")
+      } else {
+        console.log("그러나 갈 수 있는 길이 없다.")
+      }
+      return false
+    }
+    const foundPlayer = findPlayers(others, xToMove, yToMove)
+    if (foundPlayer) {
+      if (!foundPlayer.move(direction, map, players, true)) {
+        return false
+      }
+    }
+    this.point.x = xToMove
+    this.point.y = yToMove
+    return true
   }
 }
--- frontend/game/round.ts
+++ frontend/game/round.ts
@@ -12,53 +12,11 @@ export class Round {
 
     for (const turnedPlayer of turnedPlayers) {
 
-      await new Promise(resolve => setTimeout(resolve, 300)) // delay
+      await new Promise(resolve => setTimeout(resolve, 1000)) // delay
 
-      const others = this.players.filter(player => player.name !== turnedPlayer.name)
-      let xToMove: number = turnedPlayer.point.x
-      let yToMove: number = turnedPlayer.point.y
-      let foundPlayer: entity.Player | undefined
-      switch (await turnedPlayer.strategy(this.map, others)) {
-        case 0:
-          console.log(turnedPlayer.name, "은 위로 이동하기를 시전하였다.")
-          xToMove = turnedPlayer.point.x
-          yToMove = turnedPlayer.point.y - 1
-          turnedPlayer.point.d = entity.Direction.Up
-          break
-        case 1:
-          console.log(turnedPlayer.name, "은 오른쪽으로 이동하기를 시전하였다.")
-          xToMove = turnedPlayer.point.x + 1
-          yToMove = turnedPlayer.point.y
-          turnedPlayer.point.d = entity.Direction.Right
-          break
-          case 2:
-          console.log(turnedPlayer.name, "은 아래로 이동하기를 시전하였다.")
-          xToMove = turnedPlayer.point.x
-          yToMove = turnedPlayer.point.y + 1
-          turnedPlayer.point.d = entity.Direction.Down
-          break
-        case 3:
-          console.log(turnedPlayer.name, "은 왼쪽으로 이동하기를 시전하였다.")
-          xToMove = turnedPlayer.point.x - 1
-          yToMove = turnedPlayer.point.y
-          turnedPlayer.point.d = entity.Direction.Left
-          break
-      }
-      if (!this.map.isMovablePoint(xToMove, yToMove)) {
-        console.log("그러나 갈 수 있는 길이 없다.")
-        continue
-      }
-      foundPlayer = this.players.find(player => {
-        return player.point.x === xToMove
-          && player.point.y === yToMove
-      })
-      if (foundPlayer) {
-        console.log(`그러나 플레이어 ${foundPlayer.name}(이)가 막고 있어서 갈수 없다.`)
-        continue
-      }
-      turnedPlayer.point.x = xToMove
-      turnedPlayer.point.y = yToMove
-      if (this.map.isEndPoint(xToMove, yToMove)) {
+      turnedPlayer.move(await turnedPlayer.strategy(this.map, this.players), this.map, this.players)
+
+      if (this.map.isEndPoint(turnedPlayer.point.x, turnedPlayer.point.y)) {
         console.log(`게임 끝! ${turnedPlayer.name} 승리!!`)
         return turnedPlayer
       }
